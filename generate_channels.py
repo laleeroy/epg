39e20eb781fd109e97e7c4915f136f5f0bc48108
laleeroy <gabwielcastwo@gmail.com>
2025-03-07 00:31:13 +0800

    Revert "Drop some sites"
    This reverts commit 88d74af78b0b1af5854a398cc257f059d01e95ed.

--- generate_channels.py
+++ generate_channels.py
@@ -31,7 +31,7 @@ def save_channels(channels, output_file):
 
 base_directory = 'sites'
 output_file = 'updated_channels.xml'
-target_dirs = ['clickthecity.com', 'mysky.com.ph', 'osn.com']
+target_dirs = ['clickthecity.com', 'mysky.com.ph', 'osn.com', 'nowplayer.now.com', 'ontvtonight.com', 'singtel.com', 'tvtv.us', 'starhubtvplus.com']
 
 channels = collect_channels(base_directory, target_dirs)
 save_channels(channels, output_file)
